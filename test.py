524ee82f7601712628acdd238d09427700eb8747
liang <18339238714@163.com>
2020-09-30 15:48:53 +0800

    测试

---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: liang
+@license: (C) Copyright 2020, Node Supply Chain Manager Corporation Limited.
+@contact: weixuliang@sensorsdata.cn
+@time: 2020/9/30 3:48 下午
+'''
+print('hello world')
